3c14e717978e1fcf2d07d433068641f14a6f4afc
vanthinh <thinh.tran@prostech-asia.com>
2024-05-14 20:59:31 +0700

    first commit

---
+++ src/main/java/com/per/tvt_be/TvtBeApplication.java
@@ -0,0 +1,13 @@
+package com.per.tvt_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TvtBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TvtBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/per/tvt_be/TvtBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.per.tvt_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TvtBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
